a7a796ba503b697e23d5ddcbfdb7c4432f49bc40
Admin <linko@ukr.net>
2016-02-16 19:43:18 +0200

    main.go

--- main.go
+++ main.go
@@ -35,26 +35,24 @@ func main() {
 		checkError(err)
 
 		for _, v := range feed.Items {
-			if v.ID == "1184867264115104516_2322756141" {
-				fmt.Println("ID:", v.ID, "LikeCount:", v.LikeCount, "Username:", v.User.Username)
-				for _, v := range v.ImageVersions2.Candidates {
-					if v.Width == 240 && v.Height == 240 {
-						fmt.Println("<img src=\"" + v.URL + "\">")
-					}
+			fmt.Println("ID:", v.ID, "LikeCount:", v.LikeCount, "Username:", v.User.Username)
+			for _, v := range v.ImageVersions2.Candidates {
+				if v.Width == 240 && v.Height == 240 {
+					fmt.Println("<img src=\"" + v.URL + "\">")
 				}
-
-				for _, v := range v.Comments {
-					fmt.Println("<img src=\""+v.User.ProfilePicURL+"\">", v.User.Username, v.Text)
-				}
-
-				//likers, err := instagram.GetMediaLikers(v.ID)
-				//instagramError(err)
-				//fmt.Println("\tUserCount:", likers.UserCount)
-				//for _, v := range likers.Users {
-				//	fmt.Println("\t", v.Username)
-				//}
-				//likersSum += likers.UserCount
 			}
+
+			for _, v := range v.Comments {
+				fmt.Println("<img src=\""+v.User.ProfilePicURL+"\">", v.User.Username, v.Text)
+			}
+
+			likers, err := instagram.GetMediaLikers(v.ID)
+			checkError(err)
+			fmt.Println("\tUserCount:", likers.UserCount)
+			for _, v := range likers.Users {
+				fmt.Println("\t", v.Username)
+			}
+			likersSum += likers.UserCount
 		}
 
 		mediaSum += feed.NumResults
